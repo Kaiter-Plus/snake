958bf6ffb575c1c807430d8d794311356042d859
Kaiter-Plus <738406369@qq.com>
2022-04-24 21:36:29 +0800

    chore: update comments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
 /**
  * 获取某个范围的随机数
- * @param {number} min
- * @param {number} max
- * @returns 范围内的随机数
+ * 
+ * @param min 生成的最小数
+ * @param max 生成的最大数
+ * @returns [min, max] 范围内的随机数
  */
 export function getRandom(min: number, max: number): number {
   return Math.round(Math.random() * max) + min
