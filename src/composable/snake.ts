1435e56cdd99171a5d774e3f87516141818c711b
Kaiter-Plus <738406369@qq.com>
2022-05-23 22:30:32 +0800

    refactor: type

--- src/composable/snake.ts
+++ src/composable/snake.ts
@@ -1,12 +1,11 @@
-import { reactive, ref } from "vue"
+import { reactive, ref } from 'vue'
 
 // 蛇头的位置
 const snakeHeadPosition = reactive({ x: 0, y: 0 })
 // 方向
 const direction = ref<string>('')
 // 蛇的身体
-type SnakeBody = Record<string, number>
-const bodies = ref<SnakeBody[]>([])
+const bodies = ref<Record<string, number>[]>([])
 export function useSnake() {
   return {
     direction,
